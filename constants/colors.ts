cc8f16f352f0a53e8ae40e00f3e1850603564a74
Yerdos <qop6261@gmail.com>
2025-10-04 19:11:52 +0500

    delete folders

--- constants/colors.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-    PRIMARY_BACKGROUND_COLOR: "rgba(37, 37, 37, 0.95)",
-    PRIMARY_COLOR: "#2462e9",
-    SECONDARY_COLOR: "rgba(235, 235, 245, 0.3)",
-    FONT_COLOR: "#ffffff",
-}
